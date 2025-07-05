f77b687c61d6ab563c339b9dc34a23180d890e52
rrex971 <75212090+rrex971@users.noreply.github.com>
2025-07-06 01:53:28 +0530

    Migrate deprecated client creation function to newer implementation; custom request to get user tags.

--- scripts/fetchtags.py
+++ scripts/fetchtags.py
@@ -1,4 +1,4 @@
-from osu import Client
+from osu import Client, Path
 from dotenv import load_dotenv
 import os
 
@@ -8,20 +8,18 @@ load_dotenv()
 
 CLIENT_ID = int(os.getenv("OSU_CLIENT_ID"))
 CLIENT_SECRET = os.getenv("OSU_CLIENT_SECRET")
-REDIRECT_URI = os.getenv("OSU_REDIRECT_URI")
 
-client = Client.from_client_credentials(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
+client = Client.from_credentials(CLIENT_ID, CLIENT_SECRET, None)
 
-beatmap_id = 221777
+beatmap_id = 5175021 # miku farm xd
 
 
 beatmap = client.get_beatmap(beatmap_id)
 beatmapset_id = beatmap.beatmapset_id
 
-beatmapset = client.get_beatmapset(beatmapset_id)
+resp = client.http.make_request(
+    Path("get", f"beatmapsets/{beatmapset_id}", "public"),
+    limit=10
+)
+print(", ".join([x['name'] for x in resp['related_tags']]))
 
-
-tags_string = beatmapset.tags
-tags = tags_string.split() if tags_string else []
-
-print(f"Tags for beatmapset {beatmapset_id}: {tags}")
